4504fbf88479ce205b4ad33741cdd54ad0a33d98
YuriiAndrukiv <yurii.andrukhiv@gmail.com>
2026-08-25 14:38:45 +0300

    Task-5 completed.

--- task-5.js
+++ task-5.js
@@ -19,4 +19,4 @@ console.log(getAllPropValues(products, 'name')); // ['Радар', 'Сканер
 
 console.log(getAllPropValues(products, 'quantity')); // [4, 3, 7, 2]
 
-console.log(getAllPropValues(products, 'category')); // []
+console.log(getAllPropValues(products, 'category')); // [].
